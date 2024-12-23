0b8bdae94a6f14976759f06328e66209d761edad
Giovani-Bianchi <giovaniwhb@gmail.com>
2024-12-23 03:52:17 -0300

    feat: Features Section na tela Home

--- uicomponents/src/pages/home/home.jsx
+++ uicomponents/src/pages/home/home.jsx
@@ -1,8 +1,11 @@
 // Importando os Styled Components do Componente de Home
-import { CTAButton, HeroContent, Text, TitleH1 } from "./home.styles.js"
+import { CheckboxCheckmark, CheckboxContainer, CTAButton, Feature, FeatureButton, FeatureDemo, FeatureExample, Features, FeaturesRow, FeaturesText, FeaturesTitle, HeroContent, LargeFeature, PurpleLink, RadioButtonCheckmark, RadioButtonContainer, SmallFeature, Text, TitleAndText, TitleH1, TitleH2, TitleH3 } from "./home.styles.js"
 
 // Importando o Styled Components Global
 import { Container, FlexCenterDiv, Row } from "../../styles/global.styles.js"
+
+// Importações dos ícones do phosphor-icons
+import { ArrowRight } from "@phosphor-icons/react";
 
 // Importando os componentes
 import Navbar from "../../components/navbar/navbar.jsx";
@@ -23,7 +26,7 @@ function Home() {
                     <Row>
 
                         {/* Conteúdo do Hero */}
-                        <FlexCenterDiv $padding="150px 0px">
+                        <FlexCenterDiv $padding="100px 0px">
                             <HeroContent>
                                 <TitleH1>A lot of React Components in one place ready to use</TitleH1>
                                 <Text>A powerful React component library for building modern, intuitive, and responsive interfaces effortlessly.</Text>
@@ -32,6 +35,165 @@ function Home() {
                         </FlexCenterDiv>
 
                     </Row>
+                </Container>
+            </section>
+
+            {/* Features Section */}
+            <section id="features-section">
+                <Container>
+
+                    {/* Título de Features */}
+                    <FeaturesTitle>
+                        <TitleH2>FEATURES</TitleH2>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="12" viewBox="0 0 56 12" fill="none">
+                            <path d="M55.7735 6L50 0.226497L44.2265 6L50 11.7735L55.7735 6ZM0 7H50V5H0V7Z" fill="#B54BD2"/>
+                        </svg>
+                    </FeaturesTitle>
+
+                    {/* Features */}
+                    <Features>
+
+                        {/* 1° Row de Features */}
+                        <FeaturesRow>
+
+                            {/* Feature 1 - Customizable Components */}
+                            <LargeFeature>
+                                <Feature>
+
+                                    {/* Título e o Texto */}
+                                    <TitleAndText>
+                                        <TitleH3>Customizable Components</TitleH3>
+                                        <FeaturesText $fontSize="16px">Fully customizable components that adapt seamlessly to your design needs, ensuring flexibility and consistency across your projects.</FeaturesText>
+                                    </TitleAndText>
+
+                                    {/* Explicação/Representação/Demonstração */}
+                                    <FeatureDemo>
+                                        <FeatureButton $backgroundColor="var(--light)">Button</FeatureButton>
+                                        <FeatureButton $backgroundColor="var(--pure)">Button</FeatureButton>
+                                        <FeatureButton $backgroundColor="var(--medium)">Button</FeatureButton>
+                                        <FeatureButton $backgroundColor="var(--dark)">Button</FeatureButton>
+                                    </FeatureDemo>
+                                    
+                                </Feature>
+                            </LargeFeature>
+
+                            {/* Feature 2 - Reusable */}
+                            <SmallFeature>
+                                <Feature>
+
+                                    {/* Título e o Texto */}
+                                    <TitleAndText>
+                                        <TitleH3>Reusable</TitleH3>
+                                        <FeaturesText $fontSize="16px">Reusable components designed to streamline development, reducing redundancy and enhancing efficiency in every project.</FeaturesText>
+                                    </TitleAndText>
+
+                                    {/* Explicação/Representação/Demonstração */}
+                                    <FeatureDemo>
+
+                                        {/* Feature Example - Home */}
+                                        <FeatureExample>
+                                            <Text $fontSize="16px" $fontWeight="var(--fw-medium)">Home</Text>
+                                            <CheckboxContainer>
+                                                <Text $fontSize="12px" $textColor="var(--medium)">Checkbox</Text>
+                                                <input type="checkbox" />
+                                                <CheckboxCheckmark></CheckboxCheckmark>
+                                            </CheckboxContainer>
+                                        </FeatureExample>
+
+                                        {/* Ícone de Arrow Right */}
+                                        <ArrowRight size={24} style={{marginRight: 10}} />
+
+                                        {/* Feature Example - About Us */}
+                                        <FeatureExample>
+                                            <Text $fontSize="16px" $fontWeight="var(--fw-medium)">About Us</Text>
+                                            <CheckboxContainer>
+                                                <Text $fontSize="12px" $textColor="var(--medium)">Checkbox</Text>
+                                                <input type="checkbox" />
+                                                <CheckboxCheckmark></CheckboxCheckmark>
+                                            </CheckboxContainer>
+                                        </FeatureExample>
+
+                                    </FeatureDemo>
+
+                                </Feature>
+                            </SmallFeature>
+
+                        </FeaturesRow>
+
+                        {/* 2° Row de Features */}
+                        <FeaturesRow>
+                            
+                            {/* Feature 3 - Large Quantity of Options */}
+                            <SmallFeature>
+                                <Feature>
+
+                                    {/* Título e o Texto */}
+                                    <TitleAndText>
+                                        <TitleH3>Large Quantity of Options</TitleH3>
+                                        <FeaturesText $fontSize="16px">A vast selection of components and variations to cover all your interface needs, from simple elements to advanced layouts.</FeaturesText>
+                                    </TitleAndText>
+
+                                    {/* Explicação/Representação/Demonstração */}
+                                    <FeatureDemo>
+                                        
+                                        {/* Button */}
+                                        <FeatureButton $backgroundColor="var(--pure)">Button</FeatureButton>
+
+                                        {/* Radio Button */}
+                                        <RadioButtonContainer>
+                                            <Text $fontSize="12px" $textColor="var(--pure)">Radio Button</Text>
+                                            <input type="radio" />
+                                            <RadioButtonCheckmark></RadioButtonCheckmark>
+                                        </RadioButtonContainer>
+
+                                        {/* Checkbox */}
+                                        <CheckboxContainer style={{top: -2}}>
+                                            <Text $fontSize="12px" $textColor="var(--medium)">Checkbox</Text>
+                                            <input type="checkbox" />
+                                            <CheckboxCheckmark></CheckboxCheckmark>
+                                        </CheckboxContainer>
+
+                                    </FeatureDemo>
+
+                                </Feature>
+                            </SmallFeature>
+
+                            {/* Feature 4 - Beautiful Design */}
+                            <LargeFeature>
+                                <Feature>
+
+                                    {/* Título e o Texto */}
+                                    <TitleAndText>
+                                        <TitleH3>Beautiful Design</TitleH3>
+                                        <FeaturesText $fontSize="16px">Stunning, professionally crafted designs that elevate your UI and deliver a polished user experience effortlessly.</FeaturesText>
+                                    </TitleAndText>
+
+                                    {/* Explicação/Representação/Demonstração */}
+                                    <FeatureDemo>
+
+                                        {/* Button */}
+                                        <FeatureButton $backgroundColor="var(--pure)">Beautiful</FeatureButton>
+                                        
+                                        {/* Checkbox */}
+                                        <CheckboxContainer style={{top: -2}}>
+                                            <Text $fontSize="12px" $textColor="var(--medium)">Design</Text>
+                                            <input type="checkbox" checked readOnly />
+                                            <CheckboxCheckmark></CheckboxCheckmark>
+                                        </CheckboxContainer>
+
+                                    </FeatureDemo>
+
+                                </Feature>
+                            </LargeFeature>
+
+                        </FeaturesRow>
+
+                        {/* CTA Features Text */}
+                        <div className="col-md-7 text-center">
+                            <Text $fontWeight="var(--bold)">Discover a glimpse of what we offer! These are just a few of our features — take a look and explore the <PurpleLink>full library</PurpleLink> to unlock even more possibilities.</Text>
+                        </div>
+
+                    </Features>
                 </Container>
             </section>
 
--- uicomponents/src/pages/home/home.styles.js
+++ uicomponents/src/pages/home/home.styles.js
@@ -9,6 +9,7 @@ import styled from "styled-components";
 export const TitleH1 = styled.h1`
     font-size: ${props => props.$fontSize || "56px"};
     font-weight: ${props => props.$fontWeight || "var(--semi-bold)" };
+    color: ${props => props.$textColor || "var(--secondary-dark)" };
     margin: 0;
 `;
 
@@ -16,6 +17,7 @@ export const TitleH1 = styled.h1`
 export const TitleH2 = styled.h2`
     font-size: ${props => props.$fontSize || "16px"};
     font-weight: ${props => props.$fontWeight || "var(--medium)" };
+    color: ${props => props.$textColor || "var(--pure)" };
     margin: 0;
 `;
 
@@ -23,13 +25,15 @@ export const TitleH2 = styled.h2`
 export const TitleH3 = styled.h3`
     font-size: ${props => props.$fontSize || "24px"};
     font-weight: ${props => props.$fontWeight || "var(--semi-bold)" };
+    color: ${props => props.$textColor || "var(--secondary-dark)" };
     margin: 0;
 `;
 
 // Text
 export const Text = styled.p`
     font-size: ${props => props.$fontSize || "18px"};
-    font-weight: var(--regular);
+    font-weight: ${props => props.$fontWeight || "var(--regular)" };
+    color: ${props => props.$textColor || "var(--secondary-dark)" };
     margin: 0;
 `;
 
@@ -66,4 +70,247 @@ export const HeroContent = styled.div.attrs({
     className: "col-9 d-flex flex-column align-items-center text-center"
 })`
     gap: 24px;
+`;
+
+/* --------------------------------------------------------------------------------------------------------
+ * Features Section
+---------------------------------------------------------------------------------------------------------- */
+
+// Features Title
+export const FeaturesTitle = styled.div.attrs({
+    className: "d-flex flex-column"
+})`
+    margin-bottom: 50px;
+`;
+
+// Features
+export const Features = styled.div.attrs({
+    className: "d-flex flex-column align-items-center"
+})`
+    gap: 50px;
+`;
+
+// Features Row
+export const FeaturesRow = styled.div.attrs({
+    className: "row gx-4 gy-3"
+})``;
+
+// Large Feature
+export const LargeFeature = styled.div.attrs({
+    className: "col-md-7"
+})``;
+
+// Small Feature
+export const SmallFeature = styled.div.attrs({
+    className: "col-md-5"
+})``;
+
+// Feature
+export const Feature = styled.div`
+    min-height: 300px;
+
+    background-color: var(--gray);
+
+    padding: 20px 20px 50px 20px;
+
+    border-radius: 8px;
+    border: 2px solid var(--secondary-dark);
+
+    box-shadow: -4px 4px 8px 0px rgba(0, 0, 0, 0.25);
+`;
+
+// Title and Text
+export const TitleAndText = styled.div.attrs({
+    className: "d-flex flex-column"
+})`
+    gap: 20px;
+`;
+
+// Features Text (Estendendo os estilos da const Text)
+export const FeaturesText = styled(Text)`
+    min-height: 100px;
+`;
+
+// Feature Demo (Demonstração da feature)
+export const FeatureDemo = styled.div.attrs({
+    className: "d-flex align-items-center"
+})`
+    gap: 10px;
+`;
+
+// Feature Button
+export const FeatureButton = styled.button`
+    padding: 10px 20px;
+
+    border: none;
+    border-radius: 8px;
+
+    font-size: 18px;
+
+    background-color: ${props => props.$backgroundColor};
+    color: var(--lightest);
+`;
+
+// Feature Example
+export const FeatureExample = styled.div.attrs({
+    className: "d-flex flex-column"
+})`
+    gap: 4px;
+`;
+
+// Checkbox Checkmark
+export const CheckboxCheckmark = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 24px;
+    height: 24px;
+
+    border: 1px solid var(--medium);
+    border-radius: 4px;
+
+    background-color: transparent;
+
+    &:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+`;
+
+// Checkbox Container
+export const CheckboxContainer = styled.label.attrs({
+})`
+    display: block;
+    position: relative;
+    
+    padding-left: 32px;
+
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    // Desce o texto da checkbox
+    ${Text} {
+        position: relative;
+        top: 3px;
+    }
+
+    // Esconde a checkbox
+    input {
+        position: absolute;
+        width: 0;
+        height: 0;
+
+        opacity: 0;
+        cursor: pointer;
+    }
+
+    // Mostra a checkmark quando o checkbox for checked
+    & input:checked ~ ${CheckboxCheckmark}:after {
+        display: block;
+    }
+
+    // Estiliza a checkmark
+    & ${CheckboxCheckmark}:after {
+        left: 9px;
+        top: 5px;
+
+        width: 5px;
+        height: 10px;
+
+        border: solid var(--medium);
+        border-width: 0 2px 2px 0;
+        
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
+`;
+
+// RadioButton Checkmark
+export const RadioButtonCheckmark = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 18px;
+    height: 18px;
+
+    border: 1px solid var(--pure);
+    border-radius: 50%;
+
+    &:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+`;
+
+// RadioButton Container
+export const RadioButtonContainer = styled.label.attrs({
+})`
+    display: block;
+    position: relative;
+    
+    padding-left: 24px;
+
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    // Desce o texto do radiobutton
+    ${Text} {
+        position: relative;
+        top: 1px;
+    }
+
+    // Esconde o radiobutton
+    input {
+        position: absolute;
+        width: 0;
+        height: 0;
+
+        opacity: 0;
+        cursor: pointer;
+    }
+
+    // Mostra a checkmark quando o radiobutton for checked
+    & input:checked ~ ${RadioButtonCheckmark}:after {
+        display: block;
+    }
+
+    // Estiliza a checkmark
+    & ${RadioButtonCheckmark}:after {
+        top: 5px;
+        left: 5px;
+
+        width: 6px;
+        height: 6px;
+
+        background-color: var(--pure);
+
+        border-radius: 50%;
+    }
+`;
+
+// Purple Link
+export const PurpleLink = styled.span`
+    color: var(--pure);
+    text-decoration: underline;
+
+    cursor: pointer;
+
+    transition: 0.25s;
+    
+    // Hover State
+    &:hover {
+        color: var(--medium);
+        transition: 0.25s;
+    }
 `;
